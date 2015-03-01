f8426126f986188b104b0df068537b380f704dc5
karrde00 <karrde00@gmail.com>
2015-02-28 21:56:07 -0600

    Get testing up

--- public/server.rb
+++ public/server.rb
@@ -18,5 +18,12 @@ end
 post '/logs' do
   body = request.body.rewind
   data = JSON.parse request.body.read
-  "Hello #{data}"
+
+  log = new Log(
+    userid: data['userid'],
+    site_visited: data['site_visited'],
+    redirected_to: data['redirected_to'],
+    timestamp: data['timestamp']
+  );
+  log.save!
 end
